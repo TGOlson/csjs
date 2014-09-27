8fb12a26ba969a97701f51df362c48763bbe61eb
Tyler Olson <tydotg@gmail.com>
2014-09-27 01:51:34 -0700

    Comilation time notes

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var CSJS = require('./csjs');
 
 var block = {};
 
-for(var i = 0; i < 5000; i++) {
+for(var i = 0; i < 100; i++) {
   block[i] = {
     color: 'blue',
     div: {color: 'red'},
@@ -16,12 +16,24 @@ for(var i = 0; i < 5000; i++) {
 
 function timer(func) {
   var start = new Date();
-  func();
+  for(var i = 0; i < 50; i++) {
+    func();
+  }
+
   return new Date() - start;
 }
 
 CSJS.minify = true;
 
 console.log(timer(function() {
-  CSJS.compile(block);
+  var css = CSJS.compile(block);
+  // console.log(css.length);
 }));
+
+// one style sheet of 5,000 blocks
+// 350,000+ chars minified (launchpad is about 100k)
+// 384 ms to compile
+// 25% more chars un-minified
+
+// when split into 50 stylesheets of 100 blocks
+// 84 ms to compile
